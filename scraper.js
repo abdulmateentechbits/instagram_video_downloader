13438f55d15a6eaa7c2676ef5fb63df598f9e034
abdulmateentechbits <mateen@techibits.com>
2023-04-22 14:41:18 +0500

    first commit

---
+++ scraper.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const instagramGetUrl = require("instagram-url-direct")
+
+app.get('/download', async (req, res) => {
+  const url = req.query.url;
+  const post = await instagramGetUrl(url);
+  console.log(post);
+  res.send(post);
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
